d87161cbef64b912e04f1355c3125d851d1e9d3c
cbabi2023 <abijithcbbsccs2021@gmail.com>
2025-02-06 23:02:37 +0530

    Add animation to SplashScreen for a smoother transition to SignUp screen

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
-
 import 'package:to_do_app/views/sign_up_screen/signup_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -12,10 +10,29 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
+
+    // Set up the animation controller
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2), // Duration of the zoom effect
+    );
+
+    _animation = Tween<double>(begin: 0.2, end: 1.0).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    // Start the animation
+    _controller.forward();
+
+    // Navigate to the SignUp screen after the animation
     Timer(Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
@@ -25,20 +42,33 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   @override
+  void dispose() {
+    _controller.dispose(); // Always dispose of controllers
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/images/logo.png',
-              width: Adaptive.w(40),
-              height: Adaptive.h(30),
+            AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: _animation.value,
+                  child: child,
+                );
+              },
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: Adaptive.w(40),
+                height: Adaptive.h(30),
+              ),
             ),
-            SizedBox(
-              height: Adaptive.h(20),
-            ),
+            SizedBox(height: Adaptive.h(10)),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
